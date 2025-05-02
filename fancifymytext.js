c703fb1a53c124206464de61199eeb9ecf0d5bb2
FarahnazHalimiTalash <159969872+FarahnazHalimiTalash@users.noreply.github.com>
2025-05-02 15:40:21 -0700

    Add files via upload

---
+++ fancifymytext.js
@@ -0,0 +1,33 @@
+function makeTextBigger() {
+    alert("Hello, world!");
+    document.getElementById("userText").style.fontSize = "24pt";
+}
+
+function applyStyle() {
+    let textArea = document.getElementById("userText");
+    if (document.getElementById("fancy").checked) {
+        alert("FancyShmancy selected!");
+        textArea.style.fontWeight = "bold";
+        textArea.style.color = "blue";
+        textArea.style.textDecoration = "underline";
+    } else {
+        alert("BoringBetty selected!");
+        textArea.style.fontWeight = "normal";
+        textArea.style.color = "black";
+        textArea.style.textDecoration = "none";
+    }
+}
+
+function mooText() {
+    let textArea = document.getElementById("userText");
+    let text = textArea.value.toUpperCase();
+    let sentences = text.split(".");
+    for (let i = 0; i < sentences.length - 1; i++) {
+        let words = sentences[i].trim().split(" ");
+        if (words.length > 0) {
+            words[words.length - 1] += "-Moo";
+            sentences[i] = words.join(" ");
+        }
+    }
+    textArea.value = sentences.join(". ");
+}
